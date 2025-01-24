462e71808dfad178a2898bc25497da42fa54deba
Weixi (Chrisio) Guan <chris322322@gmail.com>
2025-01-24 23:26:07 +0800

    +addition: add mapper package at launch

--- controlbg/src/main/java/com/conserveit/controlbg/ControlbgApplication.java
+++ controlbg/src/main/java/com/conserveit/controlbg/ControlbgApplication.java
@@ -1,9 +1,11 @@
 package com.conserveit.controlbg;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.conserveit.controlbg.mapper")
 public class ControlbgApplication {
 
 	public static void main(String[] args) {
