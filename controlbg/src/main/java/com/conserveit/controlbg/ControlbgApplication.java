93454a2761d36882df59903ace459b15ac87dd71
Weixi (Chrisio) Guan <chris322322@gmail.com>
2025-01-24 19:52:03 +0800

    Initialise project

---
+++ controlbg/src/main/java/com/conserveit/controlbg/ControlbgApplication.java
@@ -0,0 +1,13 @@
+package com.conserveit.controlbg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControlbgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControlbgApplication.class, args);
+	}
+
+}
---
+++ controlbg/src/test/java/com/conserveit/controlbg/ControlbgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.conserveit.controlbg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControlbgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
