121b2a77c5b05d5bffaa5120144b67bf9b4afd4d
Weixi (Chrisio) Guan <chris322322@gmail.com>
2025-01-26 20:12:02 +0800

    +add: add service and controller for building

---
+++ controlbg/src/main/java/com/conserveit/controlbg/controller/BuildingController.java
@@ -0,0 +1,11 @@
+package com.conserveit.controlbg.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/building")
+@RequiredArgsConstructor
+public class BuildingController {
+}
---
+++ controlbg/src/main/java/com/conserveit/controlbg/service/impl/BuildingServiceImpl.java
@@ -0,0 +1,8 @@
+package com.conserveit.controlbg.service.impl;
+
+import com.conserveit.controlbg.service.BuildingService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BuildingServiceImpl implements BuildingService {
+}
