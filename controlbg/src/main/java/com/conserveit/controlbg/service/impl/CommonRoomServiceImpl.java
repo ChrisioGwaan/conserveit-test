ea1584813202258678130b804d9f13751a6b6ade
Weixi (Chrisio) Guan <chris322322@gmail.com>
2025-01-26 20:12:09 +0800

    +add: add service and controller for common-room

---
+++ controlbg/src/main/java/com/conserveit/controlbg/service/impl/CommonRoomServiceImpl.java
@@ -0,0 +1,8 @@
+package com.conserveit.controlbg.service.impl;
+
+import com.conserveit.controlbg.service.CommonRoomService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CommonRoomServiceImpl implements CommonRoomService {
+}
