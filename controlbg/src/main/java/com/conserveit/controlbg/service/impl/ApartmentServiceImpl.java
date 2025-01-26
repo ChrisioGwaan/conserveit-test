5b74ab9df6977544e92a4c397c0e245b0259a038
Weixi (Chrisio) Guan <chris322322@gmail.com>
2025-01-26 20:11:29 +0800

    +add: add service and controller for apartment

---
+++ controlbg/src/main/java/com/conserveit/controlbg/service/impl/ApartmentServiceImpl.java
@@ -0,0 +1,8 @@
+package com.conserveit.controlbg.service.impl;
+
+import com.conserveit.controlbg.service.ApartmentService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ApartmentServiceImpl implements ApartmentService {
+}
