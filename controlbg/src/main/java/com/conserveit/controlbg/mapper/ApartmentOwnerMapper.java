9b6b923a71a8fb47b9ddb5951d1a6947e558b884
Weixi (Chrisio) Guan <chris322322@gmail.com>
2025-01-26 19:57:10 +0800

    +add: define mappers for models Building, apartment, apartment owner, common room, room

---
+++ controlbg/src/main/java/com/conserveit/controlbg/mapper/ApartmentOwnerMapper.java
@@ -0,0 +1,9 @@
+package com.conserveit.controlbg.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.conserveit.controlbg.entity.ApartmentOwner;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface ApartmentOwnerMapper extends BaseMapper<ApartmentOwner> {
+}
---
+++ controlbg/src/main/java/com/conserveit/controlbg/mapper/CommonRoomMapper.java
@@ -0,0 +1,9 @@
+package com.conserveit.controlbg.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.conserveit.controlbg.entity.CommonRoom;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface CommonRoomMapper extends BaseMapper<CommonRoom> {
+}
