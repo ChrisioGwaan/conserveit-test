2b9448e67af1cba5986de3f6586d8f5a03c47e54
Weixi (Chrisio) Guan <chris322322@gmail.com>
2025-01-27 23:36:20 +0800

    +add: implement update current temp for apartment

--- controlbg/src/main/java/com/conserveit/controlbg/controller/ApartmentController.java
+++ controlbg/src/main/java/com/conserveit/controlbg/controller/ApartmentController.java
@@ -1,6 +1,10 @@
 package com.conserveit.controlbg.controller;
 
+import com.conserveit.controlbg.service.ApartmentService;
+import com.conserveit.controlbg.utils.R;
 import lombok.RequiredArgsConstructor;
+import org.apache.ibatis.annotations.Param;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,4 +12,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/apartment")
 @RequiredArgsConstructor
 public class ApartmentController {
+
+    private final ApartmentService apartmentService;
+
+    @PutMapping("/updateCurTemp")
+    public R updateCurrentTemperature(@Param("apartmentId") String apartmentId,
+                                      @Param("newTempStr") String newTempStr) {
+        return apartmentService.updateApartmentCurrentTemp(apartmentId, newTempStr);
+    }
+
 }
