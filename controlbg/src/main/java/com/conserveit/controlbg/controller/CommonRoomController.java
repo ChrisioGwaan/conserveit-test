90399021baa9c2657d7acc154726802f02ab90d6
Weixi (Chrisio) Guan <chris322322@gmail.com>
2025-01-27 21:26:43 +0800

    +add: implement current temp updator Todo: Need data read from sensor

--- controlbg/src/main/java/com/conserveit/controlbg/controller/CommonRoomController.java
+++ controlbg/src/main/java/com/conserveit/controlbg/controller/CommonRoomController.java
@@ -1,6 +1,10 @@
 package com.conserveit.controlbg.controller;
 
+import com.conserveit.controlbg.service.CommonRoomService;
+import com.conserveit.controlbg.utils.R;
 import lombok.RequiredArgsConstructor;
+import org.apache.ibatis.annotations.Param;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,4 +12,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/common-room")
 @RequiredArgsConstructor
 public class CommonRoomController {
+
+    private final CommonRoomService commonRoomService;
+
+    @PutMapping("/updateCurTemp")
+    public R updateCurrentTemperature(@Param("commonRoomId") String commonRoomId,
+                                      @Param("newTempStr") String newTempStr) {
+        return commonRoomService.updateCurrentTemperature(commonRoomId, newTempStr);
+    }
+
 }
--- controlbg/src/main/java/com/conserveit/controlbg/service/CommonRoomService.java
+++ controlbg/src/main/java/com/conserveit/controlbg/service/CommonRoomService.java
@@ -1,4 +1,12 @@
 package com.conserveit.controlbg.service;
 
-public interface CommonRoomService {
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.conserveit.controlbg.entity.CommonRoom;
+import com.conserveit.controlbg.utils.R;
+
+public interface CommonRoomService extends IService<CommonRoom> {
+
+    // Need a schedule to update the temperature from sensor
+    R updateCurrentTemperature(String commonRoomId, String newTempStr);
+
 }
